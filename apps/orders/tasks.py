cd7293509f3c3cbc3d452b811f8cc42700cd9a97
ordemdigitale <ordemdigitale@gmail.com>
2023-03-22 19:14:20 +0000

    Update order created task

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -15,7 +15,8 @@ def order_created(order_id):
     subject = f'Commande N°{order.id}'
     message = f'{order.name},\n\n' \
                 f'Vous avez passé votre commande avec succès. \n' \
-                f'Le numéro de la commande est le suivant {order.id}.'
+                f'Votre numéro de commande est: #{order.id}. \n' \
+                f'Le montant de votre commande est £ {order.get_total_cost()}.'
     mail_sent = send_mail(subject,
                             message,
                             'settings.DEFAULT_FROM_EMAIL', # the sender / from address
